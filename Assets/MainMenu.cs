5db06c27e31d95457ad4cc1c635dee2930477be5
nohaeila <nohaeila.laghalid@ynov.com>
2025-01-22 10:13:32 +0100

    ajout du MainMenu

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -1,21 +1,30 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
- 
+
 public class MainMenu : MonoBehaviour
 {
-    [SerializeField] private string gameSceneName = "GameScene"; 
+    [SerializeField] private string game = "game"; 
 
+    // Fonction appelée lorsque le bouton "Jouer" est cliqué
     public void PlayGame()
     {
-        SceneManager.LoadScene(gameSceneName);
+        if (Application.CanStreamedLevelBeLoaded(game))
+        {
+            SceneManager.LoadScene(game);
+        }
+        else
+        {
+            Debug.LogError($"La scène '{game}' n'existe pas ou n'est pas incluse dans les Build Settings.");
+        }
     }
 
+    // Fonction appelée lorsque le bouton "Quitter" est cliqué
     public void QuitGame()
     {
 #if UNITY_EDITOR
-            UnityEditor.EditorApplication.isPlaying = false;
+        UnityEditor.EditorApplication.isPlaying = false; // Arrête le jeu dans l'éditeur Unity
 #else
-        Application.Quit();
+        Application.Quit(); // Quitte le jeu une fois buildé
 #endif
     }
 }
